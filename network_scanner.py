b5d7b22f3b768dbf5dd9fd7d965e8cb2767c85cc
TheMeepCoder <lineapetersen.25@gmail.com>
2025-11-11 16:05:23 +0100

    Version 1.4
    - rearranged the code to be more dynamic
    - added filtered serivce state
    - added the option continue the program
    - added placeholders for next features

--- network_scanner.py
+++ network_scanner.py
@@ -8,25 +8,25 @@ Date: [20-10-2025]
 
 # Excample Ports
 # 22 - is a port that is open
-# 21 - is a port that is closed
-# 33 - is a port that dose not exist/cannot be reached
+# 25 - is a port that is closed
+# 21 - is a port that cannot be reached/filtered
 
 import ipaddress # import to help validate IP format
 import socket # allows the program to create network sockets, connect to host:port, and look up serivce names
-import datetime # logs time
+import datetime # logs time 
+import sys # adds a little flair
 
-with open("results.txt", "a") as file:  # "a" adds "w" wipes
-    file.write("") #import txt file and allows writing in it 
-
-def is_port_open(host: str, port: int, timeout: float = 1.0) -> bool: # "->" just what is expected, dosen't do anything on it's without an mypy libarey, same with the classes. here it just acts like a simple comment
+def is_port_open(host: str, port: int, timeout: float = 1.0) -> str: # "->" just what is expected, dosen't do anything on it's without an mypy libarey, same with the classes. here it just acts like a simple comment
     #Return True if port is open, else False
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # the with statement closes connection after it's done scaning which is after it has printed out the last port in the treminal and txt
         s.settimeout(timeout) # sets a timeout of 1 secound
         try:
             s.connect((host, port)) # adtempts to connect and scan a port
-            return True
-        except (socket.timeout, ConnectionRefusedError, OSError):
-            return False
+            return "open"
+        except socket.timeout:
+            return "closed"
+        except (ConnectionRefusedError, OSError):
+            return "filtered"
 
 def validate_host(prompt: str = "Host to scan (IP or hostname): ") -> str:
     #Prompt until a valid IP address or hostname is provided; return to host.
@@ -54,23 +54,21 @@ def get_service_name(port: int) -> str:
     except OSError: # OSError means when it cannot find any value
         return "unknown"
     
-def log_result(port: int, service: str, is_open: bool, host: str):
+def log_result(port: int, service: str, state: str, host: str):
     #logs result into a txt file
     with open("results.txt", "a") as file:
-        light_status = "🟢" if is_open else "🔴"  # Open in append mode, which adds new lines for each scanned port
-        status = "open" if is_open else "closed"
-        file.write(f"{light_status}Port {port} is {status}. Service: {service}. Time: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}. Hostname/IP: {host}\n")
+        light_status = {"open": "🟢", "closed": "🔴", "filterd": "⚫"}.get(state, "❔")  # Open in append mode, which adds new lines for each scanned port
+        file.write(f"{light_status}Port {port} is {state}. Service: {service}. Time: {datetime.datetime.now().strftime('%H:%M:%S')}. Hostname/IP: {host}\n")
         file.write("-------\n")
     
 def scan_single_port(host: str):
     port = int(input("Enter the port number to be scanned: "))
-    is_open = is_port_open(host, port)
+    state = is_port_open(host, port)
     service = get_service_name(port)
-    status = "open" if is_open else "closed"
-    light_status = "🟢" if is_open else "🔴"
-    log_result(port, service, is_open, host)  # calls funtion to Log the result and sends the value of the varibles
+    light_status = {"open": "🟢", "closed": "🔴", "filterd": "⚫"}.get(state, "❔")
+    log_result(port, service, state, host)  # calls funtion to Log the result and sends the value of the varibles
 
-    print(f"{light_status} Port {port} is {status}. Service: {service}.")
+    print(f"{light_status} Port {port} is {state}. Service: {service}.")
     print("-------")
     
     print("results have been saved in results.txt")
@@ -79,40 +77,106 @@ def scan_single_port(host: str):
 def scan_multi_ports(host: str, start_port: int, end_port: int):
     #Scan a range of ports between start_port and end_port
     for port in range(start_port, end_port + 1): # 
-        is_open = is_port_open(host, port)
+        state = is_port_open(host, port)
         service = get_service_name(port)
-        status = "open" if is_open else "closed"
-        light_status = "🟢" if is_open else "🔴"
-        log_result(port, service, is_open, host)  # calls funtion to Log the result and sends the value of the varibles
+        light_status = {"open": "🟢", "closed": "🔴", "filterd": "⚫"}.get(state, "❔")
+        log_result(port, service, state, host)  # calls funtion to Log the result and sends the value of the varibles
 
-        print(f"{light_status} Port {port} is {status}. Service: {service}.")
+        print(f"{light_status} Port {port} is {state}. Service: {service}.")
         print("-------")
     
     print("Results have been saved in results.txt")
     print("-------")
 
-print("🛑Remeber to ONLY scan on networks you have permission to🛑")
-print("-------")
-host = validate_host("Host to scan (ip or hostname): ") # Asks user for IP/Hostname
-print("-------")
+def normal_scan():
+    host = validate_host("Host to scan (ip or hostname): ") # Asks user for IP/Hostname
+    print("-------")
 
-while True:
-    try: 
-        choice = int(input("Select scanning mode: (1) Single-port (2) Multi-port: "))
-        print("-------")
-        
-        if choice == 1:
-            scan_single_port(host)
-            break
-        elif choice == 2:
-            start_port = int(input("Enter start port number: "))
-            end_port = int(input("Enter end port number: "))
-            scan_multi_ports(host, start_port, end_port)
-            break
-        else:
-            print("Invailed input. Please enter 1 or 2")
+    while True:
+        try: 
+            choice = int(input("Select scanning mode: (1) Single-port (2) Multi-port: "))
             print("-------")
-    except ValueError:
-        print("Invailed input. Please enter a number")
+            
+            if choice == 1:
+                with open("results.txt", "a") as file:
+                    file.write(f"Scan done to {host} at {datetime.datetime.now().strftime('%Y-%m-%d')}\n")
+                    file.write("-------\n")
+                scan_single_port(host)
+                break
+            elif choice == 2:
+                with open("results.txt", "a") as file:
+                    file.write(f"Scan done to {host} at {datetime.datetime.now().strftime('%Y-%m-%d')}\n")
+                    file.write("-------\n")
+                start_port = int(input("Enter start port number: "))
+                end_port = int(input("Enter end port number: "))
+                scan_multi_ports(host, start_port, end_port)
+                break
+            else:
+                print("Invailed input. Please enter 1 or 2")
+                print("-------")
+        except ValueError:
+            print("Invailed input. Please enter a number")
+            print("-------")
+
+
+def quick_scan():
+    print("meep")
+
+def test_localhost():
+    print("eep")
+
+def menu():
+    print("🛑Remeber to ONLY scan on networks you have permission to🛑")
+    while True:
         print("-------")
+        print("Pick yhe selection between 1 to 3")
+        print("1. Normal Scan")
+        print("2. Quick Scan")
+        print("3. Test Localhost")
+        print("-------")
+        try:
+            selection = int(input(""))
+            if selection == 1:
+                normal_scan()
+                break
+            elif selection == 2:
+                quick_scan()
+                break
+            elif selection == 3:
+                test_localhost()
+                break
+            else:
+                print("input out of range, pick a number between 1-3")
+        except ValueError:
+            print("Invailed input, please pick a number between 1-3")
+
+def close_program():
         
+    while True:
+        try:
+            done = input("do you want to go back to selection?(y/n): ")
+            if done == "y":
+                menu()
+            elif done == "n":
+                print("-------")
+                print("Thank you for using my port scanner")
+                break
+            else:
+                print("invailed input, type either in (y) for yes or (n) for no")
+        except ValueError:
+            print('invailed input, type either in (y) for yes or (n) for no')
+
+menu()
+
+close_program()
+
+"""
+TODO: 
+dynamic sys loading bar
+localhost test option
+qucik scan option
+summery
+timer
+
+"""
+#DONE: filtered service state
